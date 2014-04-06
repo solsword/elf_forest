640dc406d3e8a5f694f2f2dd6fd7ff9d6edcadc6
Peter Mawhorter <pmawhorter@gmail.com>
2014-04-06 10:26:18 -0700

    Added a double-check to the txgen unit test which re-loads the written .png from file and checks some pixel values.
    -Peter Mawhorter

--- src/unit_tests/suites/test_txgen.h
+++ src/unit_tests/suites/test_txgen.h
@@ -28,28 +28,11 @@ size_t test_txgen_template_moss(void) {
   run_grammar(&template_moss);
   write_texture_to_png(template_moss.result, "out/test/tmoss.png");
   write_texture_to_ppm(template_moss.result, "out/test/tmoss.ppm");
-  /* TODO: Turn this into a reading/writing unit test!
-  texture *result = load_texture_from_png(template_moss.filename);
-  write_texture_to_png(result, "out/test/tmoss.png");
-  write_texture_to_ppm(result, "out/test/tmoss.ppm");
-  printf("Test:\n");
-  printf(
-    "  px(0, 0) r g b: %d, %d, %d // value: 0x%08x\n",
-    px_red(tx_get_px(result, 0, 0)),
-    px_green(tx_get_px(result, 0, 0)),
-    px_blue(tx_get_px(result, 0, 0)),
-    tx_get_px(result, 0, 0)
-  );
-  printf(
-    "  px(1, 0) r g b: %d, %d, %d // value: 0x%08x\n",
-    px_red(tx_get_px(result, 1, 0)),
-    px_green(tx_get_px(result, 1, 0)),
-    px_blue(tx_get_px(result, 1, 0)),
-    tx_get_px(result, 1, 0)
-  );
-  cleanup_texture(result);
-  // */
   cleanup_grammar(&template_moss);
+  texture *doublecheck = load_texture_from_png("out/test/tmoss.png");
+  if (tx_get_px(doublecheck, 0, 0) != 0xff00b40f) { return 1; }
+  if (tx_get_px(doublecheck, 0, 1) != 0xbf10870d) { return 2; }
+  cleanup_texture(doublecheck);
   return 0;
 }
 
