fb104ffd8ca812efe39db5fdf3456f0562a18655
Peter Mawhorter <pmawhorter@gmail.com>
2016-01-28 09:56:59 -0500

    Some work on the EFD parser.
    -Peter Mawhorter

--- src/efd/efd_parser.c
+++ src/efd/efd_parser.c
@@ -6,7 +6,7 @@
  *************/
 
 // Top level parsing function that delegates to the more specific functions:
-efd_node* efd_parse_any(char const * const raw, ptrdiff_t *pos) {
+efd_node* efd_parse_any(efd_parse_state *s) {
   efd_node* result = (efd_node*) malloc(sizeof(efd_node));
   efd_parse_open(raw, pos);
   efd_check_parse_progress(pos);
@@ -17,61 +17,224 @@ efd_node* efd_parse_any(char const * const raw, ptrdiff_t *pos) {
 // Parsing functions for the EFD primitive types:
 //-----------------------------------------------
 
-efd_node* efd_parse_collection(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_collection(efd_parse_state *s);
 
-efd_node* efd_parse_proto(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_proto(efd_parse_state *s);
 
-efd_node* efd_parse_integer(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_integer(efd_parse_state *s);
 
-efd_node* efd_parse_number(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_number(efd_parse_state *s);
 
-efd_node* efd_parse_string(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_string(efd_parse_state *s);
 
-efd_node* efd_parse_obj_array(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_obj_array(efd_parse_state *s);
 
-efd_node* efd_parse_int_array(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_int_array(efd_parse_state *s);
 
-efd_node* efd_parse_num_array(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_num_array(efd_parse_state *s);
 
-efd_node* efd_parse_str_array(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_str_array(efd_parse_state *s);
 
 // Functions for parsing bits & pieces:
 //-------------------------------------
 
-void efd_parse_open(char const * const raw, ptrdiff_t *pos);
+void efd_parse_open(efd_parse_state *s);
 
-void efd_parse_close(char const * const raw, ptrdiff_t *pos);
+void efd_parse_close(efd_parse_state *s);
 
-efd_node_type efd_parse_type(char const * const raw, ptrdiff_t *pos);
+efd_node_type efd_parse_type(efd_parse_state *s);
 
-void efd_parse_name(char const * const raw, ptrdiff_t *pos, char *r_name);
+void efd_parse_name(efd_parse_state *s, char *r_name);
 
-void efd_parse_schema(char const * const raw, ptrdiff_t *pos, char *r_name);
+void efd_parse_schema(efd_parse_state *s, char *r_name);
 
-ptrdiff_t efd_parse_int(char const * const raw, ptrdiff_t *pos);
+ptrdiff_t efd_parse_int(efd_parse_state *s);
 
-float efd_parse_float(char const * const raw, ptrdiff_t *pos);
+float efd_parse_float(efd_parse_state *s);
 
 void efd_grab_string_limits(
-  char const * const raw,
+  efd_parse_state *s
   ptrdiff_t *start,
   ptrdiff_t *end
-);
+) {
+  efd_quote_state state = EFD_QUOTE_STATE_NORMAL;
+  char c;
+  char q;
 
-void efd_parse_skip(char const * const raw, ptrdiff_t *pos) {
-  // TODO: HERE (comments)
-  while (is_whitespace(raw[*pos]) && raw[*pos] != '\0') {
-    *pos += 1;
+  // skip to the starting quote:
+  efd_parse_skip(s);
+  efd_assume_parse_progress(s);
+
+  c = s->input[s->pos];
+  q = c;
+  if (!(q == '"' || q == '\'')) {
+    *start = -1;
+    *end = -1;
+    s->context = "quoted string";
+    s->error = EFD_PE_MALFORMED;
+    return;
+  }
+  s->pos += 1;
+  *start = s->pos;
+  while (!efd_parse_atend(s)) {
+    c = s->input[s->pos];
+    if (c == '\n') {
+      s->lineno += 1;
+    }
+    switch (state) {
+      default:
+      case EFD_QUOTE_STATE_NORMAL:
+        if (c == q) {
+          state = EFD_QUOTE_STATE_MAYBE_DONE;
+        }
+        break;
+      case EFD_QUOTE_STATE_MAYBE_DONE:
+        if (c == q) {
+          state = EFD_QUOTE_STATE_NORMAL;
+        } else {
+          state = EFD_QUOTE_STATE_DONE;
+        }
+        break;
+    }
+    if (state == EFD_QUOTE_STATE_DONE) {
+      break;
+    } else {
+      s->pos += 1;
+    }
+  }
+  switch (state) {
+    default:
+    case EFD_QUOTE_STATE_NORMAL:
+      s->context = "quoted string";
+      s->error = EFD_PE_INCOMPLETE;
+      break;
+    case EFD_QUOTE_STATE_MAYBE_DONE:
+    case EFD_QUOTE_STATE_DONE:
+      *end = s->pos - 2;
+      break;
+  }
+}
+
+void efd_parse_skip(efd_parse_state *s) {
+  efd_skip_state state = EFD_SKIP_STATE_NORMAL;
+  char c;
+  while (!efd_parse_atend(s)) {
+    c = s->input[s->pos];
+    if (c == '\n') {
+      s->lineno += 1;
+    }
+    switch (state) {
+      default:
+      case EFD_SKIP_STATE_NORMAL:
+        if (c == '/') {
+          state = EFD_SKIP_STATE_MAYBE_COMMENT;
+        } else if (!is_whitespace(c)) {
+          state = EFD_SKIP_STATE_DONE;
+        }
+        break;
+      case EFD_SKIP_STATE_MAYBE_COMMENT: // maybe starting a comment
+        if (c == '/') {
+          state = EFD_SKIP_STATE_LINE_COMMENT;
+        } else if (c == '*') {
+          state = EFD_SKIP_STATE_BLOCK_COMMENT;
+        } else {
+          s->pos -= 1; // go back to the '/' immediately preceding us
+          if (c == '\n') {
+            s->lineno -= 1;
+          }
+          state = EFD_SKIP_STATE_DONE; // not actually the start of a comment
+        }
+        break;
+      case EFD_SKIP_STATE_LINE_COMMENT:
+        if (c == '\n') {
+          state = EFD_SKIP_STATE_NORMAL;
+        }
+        break;
+      case EFD_SKIP_STATE_BLOCK_COMMENT: // comment until */
+        if (c == '*') {
+          s->state = EFD_SKIP_STATE_MAYBE_BLOCK_END;
+        }
+        break;
+      case EFD_SKIP_STATE_MAYBE_BLOCK_END:
+        if (c == '/') {
+          s->state = EFD_SKIP_STATE_NORMAL; // done with this block comment
+        } else {
+          s->state = EFD_SKIP_STATE_BLOCK_COMMENT; // still in a block comment
+        }
+        break;
+    }
+    if (s->state == EFD_SKIP_STATE_DONE) { // don't increment position
+      break;
+    } else { // increment our position and keep going
+      s->pos += 1;
+    }
+  }
+  if  (
+    !(s->state == EFD_SKIP_STATE_DONE
+   || s->state == EFD_SKIP_STATE_NORMAL
+   || s->state == EFD_SKIP_STATE_LINE_COMMENT)
+  ) {
+    if (
+      s->state == EFD_SKIP_STATE_BLOCK_COMMENT
+   || s->state == EFD_SKIP_STATE_MAYBE_BLOCK_END
+    ) {
+      s->context = "block comment";
+    } // otherwise leave whatever context was already there
+    s->error = EFD_PE_INCOMPLETE;
   }
 }
 
 // Helper functions:
 //------------------
 
-void efd_check_parse_progress(ptrdiff_t *pos) {
-  if (pos < 0) {
-    // TODO: Better here!!
-    fprintf(stderr, "Parsing Error!\n");
-    exit(1);
+void efd_assume_parse_progress(efd_parse_state *s) {
+  if (s->error != EFD_PE_NO_ERROR) {
+    fprintf(
+      stderr,
+      "[%.*s:%d] ",
+      EFD_PARSER_MAX_FILENAME_DISPLAY,
+      s->filename,
+      s->lineno
+    );
+    switch (s->error) {
+      default:
+      case EFD_PE_UNKNOWN:
+        s->error = EFD_PE_UNKNOWN;
+        fprintf(
+          stderr,
+          "Unknown parsing error while parsing %.*s.\n"
+          EFD_PARSER_MAX_CONTEXT_DISPLAY,
+          s->context
+        );
+        break;
+      case EFD_PE_MALFORMED:
+        fprintf(
+          stderr,
+          "Malformed input while parsing %.*s at '%.*s'.\n",
+          EFD_PARSER_MAX_CONTEXT_DISPLAY,
+          s->context,
+          EFD_PARSER_ERROR_CONTEXT, // TODO: pre-context as well?
+          (s->input + s->pos)
+        );
+        break;
+      case EFD_PE_INCOMPLETE:
+        fprintf(
+          stderr,
+          "Input ended while parsing %.*s.\n",
+          EFD_PARSER_MAX_CONTEXT_DISPLAY,
+          s->context
+        );
+        break;
+    }
+    exit(-s->error);
+  }
+}
+
+int efd_test_parse_progress(efd_parse_state *s, ptrdiff_t reset_to) {
+  if (s->error == EFD_PE_NO_ERROR) {
+    return 1;
+  } else {
+    s->pos = reset_to;
+    return 0;
   }
 }
--- src/efd/efd_parser.h
+++ src/efd/efd_parser.h
@@ -4,12 +4,69 @@
 // efd_parser.h
 // Parsing for the Elf Forest Data format.
 
+/*********
+ * Enums *
+ *********/
+
+// Different kinds of parsing errors:
+enum {
+  EFD_PE_NO_ERROR = 0,
+  EFD_PE_UNKNOWN,
+  EFD_PE_MALFORMED,
+  EFD_PE_INCOMPLETE
+} efd_parse_error_e;
+typedef enum efd_parse_error_e efd_parse_error;
+
+// States for grabbing quoted strings:
+enum {
+  EFD_QUOTE_STATE_NORMAL,
+  EFD_QUOTE_STATE_MAYBE_DONE,
+  EFD_QUOTE_STATE_DONE
+} efd_quote_state_e;
+typedef enum efd_quote_state_e efd_quote_state;
+
+// States for skipping whitespace and comments:
+enum {
+  EFD_SKIP_STATE_NORMAL,
+  EFD_SKIP_STATE_MAYBE_COMMENT,
+  EFD_SKIP_STATE_LINE_COMMENT,
+  EFD_SKIP_STATE_BLOCK_COMMENT,
+  EFD_SKIP_STATE_MAYBE_BLOCK_END,
+  EFD_SKIP_STATE_DONE
+} efd_skip_state_e;
+typedef enum efd_skip_state_e efd_skip_state;
+
+/**************
+ * Structures *
+ **************/
+
+struct efd_parse_state_s;
+typedef struct efd_parse_state_s efd_parse_state;
+
 /*************
  * Constants *
  *************/
 
-#define EFD_OPEN_NODE "[["
-#define EFD_CLOSE_NODE "]]"
+#define EFD_PARSER_MAX_FILENAME_DISPLAY 4096;
+#define EFD_PARSER_ERROR_CONTEXT 20;
+#define EFD_PARSER_MAX_CONTEXT_DISPLAY 32;
+
+#define EFD_PARSER_OPEN_NODE "[["
+#define EFD_PARSER_CLOSE_NODE "]]"
+
+/*************************
+ * Structure Definitions *
+ *************************/
+
+struct efd_parse_state_s {
+  char const * input;
+  ptrdiff_t pos;
+  ptrdiff_t input_length;
+  char const * filename;
+  ptrdiff_t lineno;
+  char const * context;
+  efd_parse_error error;
+}
 
 /********************
  * Inline Functions *
@@ -19,80 +76,96 @@ static inline int is_whitespace(char *c) {
   return c == ' ' || c == '\n' || c == '\t' || c == '\r';
 }
 
+// Checks whether the current character is NUL or beyond the end of the input
+// (if input_length is non-negative) and returns 1 if so or 0 otherwise.
+static inline int efd_parse_atend(efd_parse_state *s) {
+  return (
+    (s->input_length != 0 && s->pos >= s->input_length)
+ || s->input[s->pos] == '\0'
+  );
+}
+
 /*************
  * Functions *
  *************/
 
 // Top level parsing function that delegates to the more specific functions:
-efd_node* efd_parse_any(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_any(efd_parse_state *s);
 
 
 // Parsing functions for the EFD primitive types:
 //-----------------------------------------------
 
-efd_node* efd_parse_collection(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_collection(efd_parse_state *s);
 
-efd_node* efd_parse_proto(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_proto(efd_parse_state *s);
 
-efd_node* efd_parse_integer(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_integer(efd_parse_state *s);
 
-efd_node* efd_parse_number(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_number(efd_parse_state *s);
 
-efd_node* efd_parse_string(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_string(efd_parse_state *s);
 
-efd_node* efd_parse_obj_array(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_obj_array(efd_parse_state *s);
 
-efd_node* efd_parse_int_array(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_int_array(efd_parse_state *s);
 
-efd_node* efd_parse_num_array(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_num_array(efd_parse_state *s);
 
-efd_node* efd_parse_str_array(char const * const raw, ptrdiff_t *pos);
+efd_node* efd_parse_str_array(efd_parse_state *s);
 
 
 // Functions for parsing bits & pieces:
 //-------------------------------------
 
 // Parsers for node open/close brackets:
-void efd_parse_open(char const * const raw, ptrdiff_t *pos);
-void efd_parse_close(char const * const raw, ptrdiff_t *pos);
+void efd_parse_open(efd_parse_state *s);
+void efd_parse_close(efd_parse_state *s);
 
 // Parses a node type off of the front of the input:
-efd_node_type efd_parse_type(char const * const raw, ptrdiff_t *pos);
+efd_node_type efd_parse_type(efd_parse_state *s);
 
 // Parses a node name off of the front of the input. Puts up to
 // EFD_NODE_NAME_SIZE characters into r_name (including a NUL terminator),
 // overwriting whatever may have been there.
-void efd_parse_name(char const * const raw, ptrdiff_t *pos, char *r_name);
+void efd_parse_name(efd_parse_state *s, char *r_name);
 
 // Parses the schema name part of a 'c' or 'o' declaration, including the
 // leading ':'. Puts up to EFD_NODE_NAME_SIZE characters (including a trailing
 // NUL) into r_name, overwriting any previous contents.
-void efd_parse_schema(char const * const raw, ptrdiff_t *pos, char *r_name);
+void efd_parse_schema(efd_parse_state *s, char *r_name);
 
 // Parses an integer off of the front of the input:
-ptrdiff_t efd_parse_int(char const * const raw, ptrdiff_t *pos);
+ptrdiff_t efd_parse_int(efd_parse_state *s);
 
 // Parses a floating-point number off of the front of the input:
-float efd_parse_float(char const * const raw, ptrdiff_t *pos);
+float efd_parse_float(efd_parse_state *s);
 
-// Starts at the given starting point and finds an opening quote, setting the
-// starting point to point to the character after that quote. Then it scans
-// until it finds an (unescaped) closing quote that matches the type (single or
-// double) of the first and sets the end to point to the character before that
+// Starts at the initial position and finds an opening quote, setting the
+// starting point to point to the byte after that quote. Then it scans until it
+// finds an (unescaped) closing quote that matches the type (single or double)
+// of the first and sets the end to point to the character before that
 // position.
 void efd_grab_string_limits(
-  char const * const raw,
+  efd_parse_state *s,
   ptrdiff_t *start,
   ptrdiff_t *end
 );
 
-// Skips whitespace and comments:
-void efd_parse_skip(char const * const raw, ptrdiff_t *pos);
+// Skips whitespace and comments (// to end of line and /* to */):
+void efd_parse_skip(efd_parse_state *s);
 
 
 // Helper functions:
 //------------------
 
-void efd_check_parse_progress(ptrdiff_t *pos);
+// Checks that the most recent parsing function succeeded and if it did not,
+// prints an appropriate error and exits.
+void efd_assume_parse_progress(efd_parse_state *s);
+
+// Checks whether the most recent parsing function succeeded and if it did,
+// returns 1. If it failed, returns 0 and resets the parsing position to the
+// given reset point.
+int efd_test_parse_progress(efd_parse_state *s, ptrdiff_t reset_to);
 
 #endif // INCLUDE_EFD_PARSER_H
